3dd0f259ec2213a81ed216c8e2d2d1a4682a6622
Sapir Cohen <sapircohen77@gmail.com>
2019-06-13 19:39:06 +0300

    Update RichText2.js

--- rpsadmin/src/Common/RichText2.js
+++ rpsadmin/src/Common/RichText2.js
@@ -5,11 +5,12 @@ import {Form,Col,Row} from 'react-bootstrap';
 
 
 export default class RichText extends React.Component {
+  
     render() {
       return (
         <Form.Group as={Row}>
         <Col dir='rtl' sm="10">
-            <ReactQuill value={this.props.defaultInput}  onChange={(value) =>this.props.ChangeInputTextarea(value,this.props.InputTitle)} />
+            <ReactQuill modules={modules} formats={formats} value={this.props.defaultInput}  onChange={(value) =>this.props.ChangeInputTextarea(value,this.props.InputTitle)} />
         </Col>
         <Form.Label style={{textAlign:'right'}} column sm="2">
         {this.props.IsMandatory&&<span style={{color:'red'}}>*</span>}
@@ -20,3 +21,20 @@ export default class RichText extends React.Component {
       )
     }
   }
+
+ const modules = {
+    toolbar: [
+      [{ 'header': [1, 2, 3,4,false] }],
+      ['bold', 'italic', 'underline','strike', 'blockquote'],
+      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'},{ 'direction': 'rtl' },{ 'align': [] }],
+      ['link'],
+      ['clean']
+    ],
+  }
+
+ const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent','direction','align',
+    'link', 'image'
+  ]
